49a781fe5e9d1269d8ac03ce471ce022c601c593
lsh23 <shseoul14@gmail.com>
2019-04-23 21:17:23 +0900

    Edit annotation

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -14,7 +14,7 @@ public class CalculatorTest {
 
     @Mock
     private Calculator mockcalculator;
-    Calculator calculator;
+    private Calculator calculator;
 
 
     @Before
@@ -27,11 +27,11 @@ public class CalculatorTest {
     //각각 테스트 함수명 바꿔서 , 추가해서 테스트함수 작성할 것 ,단 맨앞에 Test 붙일 것
     //ex)
     //0으로나누는 것을 막기위해서 throw Exception을 던지는 것을 확인하는 테스트함수명
-    //TestThrowExeptionWhenDivideToZeRo 이런식으로 의미있게.
+    //testThrowExeptionWhenDivideToZeRo 이런식으로 의미있게.
 
 
     @Test
-    public void TestSimpleAdd() {
+    public void testSimpleAdd() {
         double a = 24;
         double b = 3;
         assertThat(calculator.add(a, b), is(27.0));
